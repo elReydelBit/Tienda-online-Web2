d66ed5b922012694351213632d60250d7e659920
elReydelBit <162018332+elReydelBit@users.noreply.github.com>
2024-10-21 15:07:15 +0200

    Delete src.1 directory
    Es una carpeta auxiliar de src

--- src.1/app/api/shop/route.ts
+++
@@ -1,9 +0,0 @@
-// src/app/api/products/route.ts
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma'; // Asegúrate de que esta ruta sea correcta
-
-// Obtener todos los productos
-export async function GET() {
-    const products = await prisma.product.findMany();
-    return NextResponse.json(products);
-}
